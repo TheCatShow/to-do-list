058ebc4c8e2f9f2fc7ba4641d4d3f04d71ebaf8d
TheCatShow <75573235+TheCatShow@users.noreply.github.com>
2023-03-25 13:05:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const li = document.createElement("li");
-const div = document.createElement("div");
-const i = document.createElement("i");
-const item = document.createElement("item");
 
 let input = document.querySelector('input');
 let text = document.querySelector('text');
 let addBtn = document.querySelector('#addBtn');
 let list = document.querySelector('#list');
 let container = document.querySelector('container');
-
-li.classList.add('listElement');
-div.classList.add('item__btns');
-i.classList.add('fa-regular');
-i.classList.add('fa-solid');
-i.classList.add('fa-trash-can');
-i.classList.add('fa-square-check');
+let item = document.createElement('item');
 
 addBtn.addEventListener('click', function (event) {
+    let li = document.createElement('li');
+    li.classList.add('item');
     event.preventDefault();
     list.append(input.value);
-})
+    
+    let div = document.createElement('div');
+    div.append(item);
+    div.classList.add('item__btns');
 
-i.append('item__btns');
-item.classList.toggle('done');
+    let i = document.createElement('i');
+    i.classList.add('fa-regular fa-square-check');
+    i.append(item__btns);
+    item.classList.toggle(done);
 
-list.addEventListener('click', function (event) {
-    list.removeChild(item__btns);
+    let l = document.createElement('l');
+    l.classList.add('fa-solid fa-trash-can');
+    l.append(item__btns);
+    ul.removeChild(li);
 })
